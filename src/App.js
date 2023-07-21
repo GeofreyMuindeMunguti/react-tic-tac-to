a48d4baa8074e6ca07f1694a9a25d29d3652cc3e
Muinde Geofrey <muindegeofrey@gmail.com>
2023-07-21 17:01:13 +0300

    setup tick tac toe game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Square from './components/Square';
 
 function App() {
+  function handleOnClick(){
+    console.log("Clicked")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='board'>
+      <div className='board-row'>
+        <Square onSquareClick={() => handleOnClick()}/>
+        <Square onSquareClick={() => handleOnClick()}/>
+        <Square onSquareClick={() => handleOnClick()}/>
+      </div>
+      <div className='board-row'>
+        <Square onSquareClick={() => handleOnClick()}/>
+        <Square onSquareClick={() => handleOnClick()}/>
+        <Square onSquareClick={() => handleOnClick()}/>
+      </div>
+      <div className='board-row'>
+        <Square onSquareClick={() => handleOnClick()}/>
+        <Square onSquareClick={() => handleOnClick()}/>
+        <Square onSquareClick={() => handleOnClick()}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Square.js
@@ -0,0 +1,6 @@
+export default function Square({onSquareClick}) {
+    return (
+        <button onClick={onSquareClick} className="square">
+        </button>
+    )
+}
